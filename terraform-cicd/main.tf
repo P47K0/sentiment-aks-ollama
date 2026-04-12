9ef5a84c44ada063ca3c526170e9a4228d723d49
Patrick Koorevaar <patrick@q-vend.nl>
2026-04-12 23:14:35 +0200

    Fixed vars in terraform-cicd

--- terraform-cicd/main.tf
+++ terraform-cicd/main.tf
@@ -8,8 +8,8 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "tfstatep47k0"
+    resource_group_name  = var.resource_group_name
+    storage_account_name = var.storage_account_name
     container_name       = "tfstate"
     key                  = "sentiment-aks-ollama.tfstate"
   }
--- terraform-cicd/variables.tf
+++ terraform-cicd/variables.tf
@@ -2,6 +2,10 @@ variable "resource_group_name" {
   type = string
 }
 
+variable "storage_account_name" {
+  default = "tfstateaz400"
+}
+
 variable "cluster_name" {
   default = "aks-ollama-dev"
 }
