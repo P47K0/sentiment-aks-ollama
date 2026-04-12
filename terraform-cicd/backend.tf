e3625baccba60425b8f5abc8ce7c2523fc1fb249
Patrick Koorevaar <patrick@q-vend.nl>
2026-04-12 23:23:21 +0200

    Try to fix  Infrastructure stage

--- terraform-cicd/backend.tf
+++ terraform-cicd/backend.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "tfstatep47k0"
-    container_name       = "tfstate"
-    key                  = "sentiment-aks-ollama.tfstate"
-  }
+  backend "azurerm" {}
 }
