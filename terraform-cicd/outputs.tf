f6021c7f9840c3b593aab75da57692138249ee67
Patrick Koorevaar <patrick@q-vend.nl>
2026-04-19 16:16:23 +0200

    Commented out custom ip in TF

--- terraform-cicd/outputs.tf
+++ terraform-cicd/outputs.tf
@@ -1,15 +1,15 @@
-output "aks_public_ip" {
-  value = azurerm_public_ip.traefik.ip_address
-}
+#output "aks_public_ip" {
+#  value = azurerm_public_ip.traefik.ip_address
+#}
 
 output "aks_cluster_name" {
   value = azurerm_kubernetes_cluster.aks.name
 }
 
-output "aks_pip_name" {
-  value = azurerm_public_ip.traefik.name
-}
+#output "aks_pip_name" {
+#  value = azurerm_public_ip.traefik.name
+#}
 
-output "aks_pip_rg_name" {
-  value = azurerm_public_ip.traefik.resource_group_name
-}
+#output "aks_pip_rg_name" {
+#  value = azurerm_public_ip.traefik.resource_group_name
+#}
